883c114a368cec4ec30f59e6015f383dd982efea
matheuswww <teteus.30.08.07@outlook.com>
2025-09-03 10:05:51 -0300

    backend: decoding cookie in websocket request

--- backend/src/cookie/cookie.ts
+++ backend/src/cookie/cookie.ts
@@ -27,26 +27,24 @@ export function validateCookie(req: Request): string {
   const value = cookies[cookieName];
 
   if (!value) throw new CookieError();
-
+  
   if (!value.startsWith('s:')) throw new CookieError();
-
   const id = signature.unsign(value.slice(2), secret);
   if (!id) throw new CookieError();
-
+  
   return id;
 }
 
 export function validateWSCookie(req: IncomingMessage): string {
+  
   if (!req.url) throw new CookieError();
 
   const url = parseUrl(req.url, true);
   const value = url.query[cookieName] as string | undefined;
-  console.log(value)
-  if (!value) throw new CookieError();
-  if (!value.startsWith("s:")) throw new CookieError();
-
-  const id = signature.unsign(value.slice(2), secret);
+  if (!value) throw new CookieError()
+  const decoded = decodeURIComponent(value)
+  if (!decoded.startsWith("s:")) throw new CookieError();
+  const id = signature.unsign(decoded.slice(2), secret);
   if (!id) throw new CookieError();
-
   return id;
 }
