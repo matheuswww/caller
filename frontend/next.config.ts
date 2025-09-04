d6f4595eed9089f53bfb5103817cdfb3efba026a
matheuswww <teteus.30.08.07@outlook.com>
2025-09-04 09:38:45 -0300

    frontend: added public env file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = process.env.NEXT_PUBLIC_TLS_REJECT_UNAUTHORIZED
 const nextConfig: NextConfig = {
   images: {
-    domains: ['192.168.0.106'],
+    domains: ['192.168.0.106', 'localhost'],
   },
 };
 export default nextConfig;
